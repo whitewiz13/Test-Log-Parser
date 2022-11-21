46a0d03f863f84f949fcb794d00f456a152927d7
whitewiz13 <sandeepthakur130197@gmail.com>
2022-11-21 22:27:01 +0530

    [Added] Parse logs logic

--- src/api/v1/Controllers/logParserController.ts
+++ src/api/v1/Controllers/logParserController.ts
@@ -1,23 +1,62 @@
 import { NextFunction, Request, Response } from "express";
-import multer from 'multer';
-
-const upload = multer.memoryStorage();
+import readline from 'readline';
+import stream from 'stream';
 
 export default class LogParserController {
 
-    public parseLogs(req: Request, res: Response, next: NextFunction) {
+    public async parseLogs(req: Request, res: Response, next: NextFunction) {
         try {
-            let logData = req?.file?.buffer.toString('utf8') || "";
-            console.log(logData);
-            for (let char of logData) {
-                //console.log(logData[i]);
+            var bufferStream = new stream.PassThrough();
+            let parsedData: { timestamp: number; loglevel: string; transactionId: string; }[] = [];
+            let count = 0;
+            let invalidCount = 0;
+            let invalidLines = [];
+
+            bufferStream.end(req?.file?.buffer);
+            var rl = readline.createInterface({
+                input: bufferStream,
+            });
+            for await (const line of rl) {
+                count++;
+                if (!line) {
+                    continue;
+                }
+                if (!line.split('- ')[1] || !line.split('- ')[2] || !line.split('- ')[0]) {
+                    //Ignore invalid data
+                    invalidCount++;
+                    invalidLines.push(count);
+                    continue;
+                }
+                let logLevel = line.split('- ')[1].trim();
+                if (logLevel === "error" || logLevel === "warn") {
+                    try {
+                        let date = line.split('- ')[0].trim();
+                        let jsonData = JSON.parse(line.split('- ')[2].trim());
+                        let item = {
+                            timestamp: Math.floor(new Date(date).getTime() / 1000),
+                            loglevel: logLevel,
+                            transactionId: jsonData?.transactionId,
+                            err: jsonData?.err
+                        }
+                        parsedData.push(item);
+                    } catch (error: any) {
+                        //Ignore invalid JSON
+                        invalidCount++;
+                        invalidLines.push(count);
+                    }
+                }
             }
-            return res.status(200).json(logData);
-        } catch (error) {
+            let dataToSend = {
+                data: parsedData,
+                status: "SUCCESS",
+                message: `Data fetched successfully (Invalid logs found ${invalidCount} at lines ${invalidLines.map((i) => i)})`
+            }
+            return res.status(200).json(dataToSend);
+        } catch (error: any) {
             let dataToSend = {
                 data: null,
                 status: "ERROR",
-                message: error
+                message: error?.message
             }
             return res.status(500).json(dataToSend);
         }
